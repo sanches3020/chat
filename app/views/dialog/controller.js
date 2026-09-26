64272179eb30c7cdf3ca373daf626ee50af94382
Mike Haiduk <x29a100@gmail.com>
2026-09-26 13:56:24 +0300

    init db

--- app/views/dialog/controller.js
+++ app/views/dialog/controller.js
@@ -1,3 +1,9 @@
-app.controller('dialog', function ($scope, $state, $stateParams) {
+app.controller('dialog', function ($scope, $state, $stateParams, api) {
     $scope.dialog_id = $stateParams.dialog_id
+
+    api.selectFile().then(function (file) {
+        api.upload('api/upload', {}, file).then(function (result) {
+
+        })
+    })
 })
---
+++ schema/01_users_dialogs_messages_likes.sql
@@ -0,0 +1,121 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.3
+-- https://www.phpmyadmin.net/
+--
+-- Хост: sof-chat-mysql-1
+-- Время создания: Сен 26 2026 г., 10:54
+-- Версия сервера: 5.7.44
+-- Версия PHP: 8.3.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `chat`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `dialogs`
+--
+
+CREATE TABLE `dialogs` (
+                           `dialog_id` int(11) NOT NULL,
+                           `dialog_title` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `likes`
+--
+
+CREATE TABLE `likes` (
+                         `like_id` int(11) NOT NULL,
+                         `message_id` int(11) NOT NULL,
+                         `word_id` int(11) NOT NULL,
+                         `like_amount` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `messages`
+--
+
+CREATE TABLE `messages` (
+                            `message_id` int(11) NOT NULL,
+                            `dialog_id` int(11) NOT NULL,
+                            `user_id` int(11) NOT NULL,
+                            `message_text` varchar(1000) NOT NULL,
+                            `message_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+                         `user_id` int(11) NOT NULL,
+                         `user_name` varchar(64) NOT NULL,
+                         `user_image` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `dialogs`
+--
+ALTER TABLE `dialogs`
+    ADD PRIMARY KEY (`dialog_id`);
+
+--
+-- Индексы таблицы `likes`
+--
+ALTER TABLE `likes`
+    ADD PRIMARY KEY (`like_id`);
+
+--
+-- Индексы таблицы `messages`
+--
+ALTER TABLE `messages`
+    ADD PRIMARY KEY (`message_id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `dialogs`
+--
+ALTER TABLE `dialogs`
+    MODIFY `dialog_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `likes`
+--
+ALTER TABLE `likes`
+    MODIFY `like_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `messages`
+--
+ALTER TABLE `messages`
+    MODIFY `message_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
